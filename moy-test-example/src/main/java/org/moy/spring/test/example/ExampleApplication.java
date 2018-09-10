069482a505418566eee18d00435f0a965e431013
yexiangyang <yexiangyang@cn.wilmar-intl.com>
2018-09-10 20:44:17 +0800

    test example

---
+++ moy-test-example/src/main/java/org/moy/spring/test/example/ExampleApplication.java
@@ -0,0 +1,19 @@
+package org.moy.spring.test.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p> 功能测试
+ * Created on 2018/9/10
+ *
+ * @author 叶向阳
+ * @since 1.0
+ */
+@SpringBootApplication
+public class ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class);
+    }
+}
